bb77008081150b321b12c92a769b8f3a4d84cba0
Felipe Santana <felipe012santana012@gmail.com>
2023-09-14 20:50:29 -0300

    Implementado DoCheck para organizar item para baixo quando selecionado

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,10 +6,13 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit{
-  ngOnInit(): void {
-    
+export class TodoListComponent implements DoCheck{
+
+  ngDoCheck(){
+    this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked));
+
   }
+  
 
   public taskList: Array<TaskList>= [];
 
