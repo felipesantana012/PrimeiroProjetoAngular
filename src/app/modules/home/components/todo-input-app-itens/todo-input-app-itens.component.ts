a8e8ba4790c2e5bb2ced4d397bf8d79ad0ceefc0
Felipe Santana <felipe012santana012@gmail.com>
2023-09-14 20:34:51 -0300

    adicionado os dados na lista

--- src/app/modules/home/components/todo-input-app-itens/todo-input-app-itens.component.ts
+++ src/app/modules/home/components/todo-input-app-itens/todo-input-app-itens.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-app-itens',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-input-app-itens.component.scss']
 })
 export class TodoInputAppItensComponent {
+
+  @Output() public emitItemTaskList = new EventEmitter();
+
+  public addItemTaskList: string = "";
+
+  public submitItemTaskList(){
+    
+   this.addItemTaskList = this.addItemTaskList.trim();
+
+    if(this.addItemTaskList){
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
+     
+    
+    
+  }
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -11,10 +11,13 @@ export class TodoListComponent implements OnInit{
     
   }
 
-  public taskList: Array<TaskList>= [
-    {task:"Minha nova Task", checked: true},
-    {task:"Minha nova Task 2 ", checked: false},
-  ];
+  public taskList: Array<TaskList>= [];
+
+public setEmitTaskList(event: string){
+  this.taskList.push({task: event, checked: false})
+}
+
+
 public deleteItemTaskList(event: number){
   this.taskList.splice(event,1);
 }
